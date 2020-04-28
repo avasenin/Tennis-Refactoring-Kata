ed899b70ab27c507b50d1b4e6d733ff6b7eef776
Andrey Vasenin <andrey.vasenin@careem.com>
2020-04-29 00:27:07 +0200

    Inline winning player

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -65,19 +65,20 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getEndGameScore() {
-        String score;
-        int minusResult = m_score1 - m_score2;
+        String score = "";
+        int differenceInScore = m_score1 - m_score2;
 
-        if (minusResult == 1) {
-            score = "Advantage player1";
-        } else if (minusResult == -1) {
-            score = "Advantage player2";
-        } else if (minusResult >= 2) {
-            score = "Win for player1";
-        } else {
-            score = "Win for player2";
+        boolean isSmallAdvantage = Math.abs(differenceInScore) == 1;
+        if (isSmallAdvantage) {
+            score = "Advantage " + getWinningPlayer();
+        } else if (Math.abs(differenceInScore) >= 2) {
+            score = "Win for " + getWinningPlayer();
         }
         return score;
+    }
+
+    private String getWinningPlayer() {
+        return m_score1 > m_score2 ? player1Name : player2Name;
     }
 
     private boolean isDraw() {
