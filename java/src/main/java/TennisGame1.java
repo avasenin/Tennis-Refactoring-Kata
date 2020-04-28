d13d7858a4573953900ff73f9e2d948b82662233
Andrey Vasenin <andrey.vasenin@careem.com>
2020-04-29 00:04:26 +0200

    Apply formatter

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,6 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -12,46 +12,50 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals("player1")) {
             m_score1 += 1;
-        else
+        } else {
             m_score2 += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
+        int tempScore = 0;
+        if (isDraw()) {
             score = getDrawScore();
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            } else if (minusResult == -1) {
+                score = "Advantage player2";
+            } else if (
+                    minusResult >= 2) {
+                score = "Win for player1";
+            } else {
+                score = "Win for player2";
+            }
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) {
+                    tempScore = m_score1;
+                } else {
+                    score += "-";
+                    tempScore = m_score2;
+                }
+                switch (tempScore) {
                     case 0:
-                        score+="Love";
+                        score += "Love";
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score += "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score += "Forty";
                         break;
                 }
             }
@@ -59,21 +63,24 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
+    private boolean isDraw() {
+        return m_score1 == m_score2;
+    }
+
     private String getDrawScore() {
         String score;
-        switch (m_score1)
-        {
+        switch (m_score1) {
             case 0:
-                    score = "Love-All";
+                score = "Love-All";
                 break;
             case 1:
-                    score = "Fifteen-All";
+                score = "Fifteen-All";
                 break;
             case 2:
-                    score = "Thirty-All";
+                score = "Thirty-All";
                 break;
             default:
-                    score = "Deuce";
+                score = "Deuce";
                 break;
 
         }
